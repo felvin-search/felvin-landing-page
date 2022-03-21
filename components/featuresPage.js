7d7ee34cad508afc471525df9bae584bd2c6c0d7
N-Shar-ma <bablinaneh@gmail.com>
2022-03-21 21:35:51 +0530

    almost complete features page - gradients done + responsive

--- components/featuresPage.js
+++ components/featuresPage.js
@@ -7,18 +7,18 @@ const FeaturesPageStyles = styled.div`
   flex-direction: column;
   justify-content: start;
   align-items: center;
-  background: radial-gradient(at top left, lightpink, lightyellow);
+  background: radial-gradient(ellipse at top left, hsla(326, 100%, 95%, 1) 0%, hsla(326, 100%, 95%, 1) 50%, hsla(60, 100%, 95%, 1) 100%);
 `
 
 const FeaturesHeading = styled.span`
   align-self: flex-start;
-  background: linear-gradient(to right, lightgreen, yellow, hotpink, deeppink, crimson);
+  background: linear-gradient(90deg, #29EF8A -21.97%, #FFD542 18.69%, #F1B1CD 55.53%, #FA3B5E 100%);
   font-size: 10rem;
   margin: 2rem;
   -webkit-text-stroke: 4px transparent;
   -webkit-background-clip: text;
   background-clip: text;
-  color: lightpink;
+  color: hsla(326, 100%, 95%, 1);
   @media (max-width: 650px) {
   font-size: 6rem;
   margin: 1.5rem;
@@ -34,9 +34,13 @@ const FeaturesAreaStyles = styled.div`
   flex-direction: row;
   justify-content: center;
   align-items: center;
+  gap: 5vw;
+  padding: 5vw 0;
+  @media (max-width: 900px) {
+      gap: 2vw;
+  }
   @media (max-width: 650px) {
     flex-direction: column;
-    /* margin: 2rem 0rem 1rem 0rem; */
   }
 `;
 
@@ -44,15 +48,22 @@ const FeatureBox = styled.div`
     border-radius: 5rem;
     width: 25vw;
     height: 50vh;
-    margin: 15px;
-    background: linear-gradient(to bottom right, gold, yellow, lightpink, pink);
+    background-image: radial-gradient(farthest-corner at 75px 100px, hsla(326, 100%, 80%, 1) 0%, hsla(326, 100%, 80%, 1) 20%, hsla(55, 100%, 80%, 1) 100%);
     @media (max-width: 900px) {
       width: 30vw;
     }
     @media (max-width: 650px) {
-      width: 80vw;
+      width: clamp(200px, 400px, 80vw);
     }
 `;
+
+const FeatureBox2 = styled(FeatureBox)`
+    background-image: radial-gradient(farthest-corner at 75px 100px, hsla(55, 100%, 80%, 1) 0%, hsla(60, 100%, 80%, 1) 20%, hsla(326, 100%, 80%, 1) 100%);
+`
+
+const FeatureBox3 = styled(FeatureBox)`
+    background-image: radial-gradient(circle at center, hsla(55, 100%, 80%, 1) 0%, hsla(326, 100%, 80%, 1) 100%);
+`
 
 const EmptyHalf = styled.div`
   height: 50%;
@@ -74,20 +85,20 @@ export default function FeaturesPage() {
               <p>Be it finding code, comparing cars or find your next favourite holiday destination, with Felvin you won’t have to go anywhere else.</p>
             </FeatureDescription>
           </FeatureBox>
-          <FeatureBox>
+          <FeatureBox2>
             <EmptyHalf />
             <FeatureDescription>
               <h2>Community Driven</h2>
               <p>Open source repo here and the community knows the best and can drive the best result.</p>
             </FeatureDescription>
-          </FeatureBox>
-          <FeatureBox>
+          </FeatureBox2>
+          <FeatureBox3>
             <EmptyHalf />
             <FeatureDescription>
               <h2>Truly Extensible</h2>
               <p>Wished you could improve your search experience? Now we give this power to you.</p>
             </FeatureDescription>
-          </FeatureBox>
+          </FeatureBox3>
         </FeaturesAreaStyles>
     </FeaturesPageStyles>);
 }
