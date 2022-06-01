b11b12275d623b0402c027ab5dc723142de566b4
Gyana Ranjan panda <83230804+lonewolf2002@users.noreply.github.com>
2022-06-01 19:58:56 +0530

    initial heropage Changes!

--- components/home.js
+++ components/home.js
@@ -4,6 +4,7 @@ import WishlistBar from "./Wishlist";
 import ReactPlayer from "react-player";
 import { Grid } from "@mui/material";
 import { motion } from "framer-motion";
+import DoneIcon from "@mui/icons-material/Done";
 const HomeStyle = styled.div`
   min-height: 100vh;
   background: radial-gradient(
@@ -20,6 +21,15 @@ const HeroStyle = styled.div`
   overflow: hidden;
   display: flex;
   margin-top: 5rem;
+  @media (max-width: 650px) {
+    margin-top: 3rem;
+  }
+  @media (max-width: 450px) {
+    margin-top: 2rem;
+  }
+  @media (max-width: 350px) {
+    margin-top: 0.5rem;
+  }
   justify-content: center;
   align-items: center;
 `;
@@ -57,9 +67,33 @@ const HeroTextTransparent = styled(HeroTextSolid)`
 const ReactPlayerStyle = styled(ReactPlayer)`
   height: 100%;
   width: 100%;
+  z-index: 3;
+  position: relative;
   margin-top: 1rem;
+  max-width: 1153px;
+  aspect-ratio: 2306 / 1372;
 `;
-
+const Tagline = styled.h2`
+  font-size: 1rem;
+  max-width: 300px;
+  margin: 0;
+  color: #232323;
+  line-height: 1.5rem;
+  display: flex;
+  align-items: center;
+`;
+const VideoBg = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  background: rgba(255, 255, 255, 0.25);
+  /* box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37); */
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.3);
+`;
 // TODO: Make the image come out fine
 export default function Home() {
   return (
@@ -72,10 +106,25 @@ export default function Home() {
               <HeroTextTransparent>The</HeroTextTransparent>
               <HeroTextTransparent>Ultimate</HeroTextTransparent>
               <HeroTextSolid>Dev Utility</HeroTextSolid>
+              <Tagline>
+                <DoneIcon />
+                72+ carefully crafted developer tools
+              </Tagline>
+
+              <Tagline>
+                <DoneIcon />
+                Available as browser extension
+              </Tagline>
+
+              <Tagline>
+                <DoneIcon />
+                All in One browser extension
+              </Tagline>
             </HeroText>
             <WishlistBar />
           </Grid>
-          <Grid item xs={12} sm={6}>
+          <Grid item xs={12} sm={6} sx={{ position: "relative",mt:2 }}>
+            <VideoBg />
             <ReactPlayerStyle
               loop={true}
               muted={true}
