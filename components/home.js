3945174c2eef7b93a33a5dc54ed78270c2ad8cdb
Gyana Ranjan panda <83230804+lonewolf2002@users.noreply.github.com>
2022-06-01 14:18:58 +0530

    initial commit

--- components/home.js
+++ components/home.js
@@ -1,20 +1,11 @@
 import styled from "styled-components";
-import Logo from "./Logo";
 import Header from "./header";
-import HeroArea from "./heroArea";
-import { motion, useAnimation } from "framer-motion";
-import { useEffect, useState } from "react";
-import updateSheetValues from "../helpers/updateSheet";
 import WishlistBar from "./Wishlist";
-
-const LinkExternal = styled.a`
-  text-decoration: none;
-`;
-
+import ReactPlayer from "react-player";
+import { Grid } from "@mui/material";
+import { motion } from "framer-motion";
 const HomeStyle = styled.div`
-  /* font-size: 50px; */
   min-height: 100vh;
-  /* background-color: yellow; */
   background: radial-gradient(
     200.18% 300.33% at 0% -48.51%,
     #2cd687 0%,
@@ -22,55 +13,23 @@ const HomeStyle = styled.div`
     #ffbfdb 58.85%,
     #fc4b6b 100%
   );
-  /* background-image: url("bg_blob.png"); */
-  display: flex;
-  flex-direction: column;
 `;
 
 const HeroStyle = styled.div`
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  flex: 1 0 auto;
   z-index: 10;
   overflow: hidden;
-  /* & canvas {
-    height: 600px;
-    width: 600px;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  } */
-`;
-
-const BackgroundSpline1 = styled.iframe`
-  pointer-events: none;
-  z-index: -1;
-  position: absolute;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  width: 100%;
-  height: 100%;
-  & canvas {
-    position: absolute;
-    left: 50%;
-  }
-`;
-const IframeContainer = styled.div`
-  position: absolute;
-  overflow: hidden;
-  width: 70%;
-  padding-top: 75%;
+  display: flex;
+  margin-top: 5rem;
+  justify-content: center;
+  align-items: center;
 `;
 
 const HeroText = styled.div`
   font-size: 3rem;
   color: #232323;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
   @media (max-width: 650px) {
     font-size: 2rem;
   }
@@ -95,64 +54,10 @@ const HeroTextTransparent = styled(HeroTextSolid)`
   }
 `;
 
-const Tagline = styled.h2`
-  font-size: 1rem;
-  max-width: 300px;
-  margin: 0;
-  color: #232323;
-  line-height: 1.5rem;
-`;
-
-const SearchBox = styled.input`
-  padding: 1.5rem;
-  padding-right: 55px;
-  font-size: 1rem;
-  border-radius: 50px;
-  width: clamp(45vw, 350px, 90vw);
-  margin: 2.5rem 0;
-  @media (max-width: 450px) {
-    padding: 1rem;
-    padding-right: 55px;
-  }
-`;
-
-const SearchButton = styled.button`
-  background: none;
-  border: none;
-  margin: 0;
-  cursor: pointer;
-  position: absolute;
-  right: 1rem;
-  top: 3.6rem;
-  opacity: 0.3;
-  transition: opacity 200ms ease-in-out;
-  @media (max-width: 450px) {
-    top: 3.1rem;
-    right: 0.8rem;
-  }
-  &:hover {
-    opacity: 0.6;
-  }
-`;
-
-const SearchButtonIcon = styled.svg`
-  height: 36px;
-  width: 36px;
-`;
-const Iframe = styled.iframe`
-  height: 600px;
-  width: 600px;
-  position: absolute;
-  z-index: -1;
-  border: none;
-  pointer-events: none;
-  @media (max-width: 600px) {
-    height: 100%;
-    width: 100%;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
+const ReactPlayerStyle = styled(ReactPlayer)`
+  height: 100%;
+  width: 100%;
+  margin-top: 1rem;
 `;
 
 // TODO: Make the image come out fine
@@ -161,18 +66,26 @@ export default function Home() {
     <HomeStyle>
       <Header />
       <HeroStyle>
-        <Iframe
-          height="100%"
-          width="100%"
-          src="https://my.spline.design/scene-4c1f8b8ea6669d4e8fa0c0f82115a3e9/"
-        />
-        <HeroText>
-          <HeroTextTransparent>Search Engine</HeroTextTransparent>
-          <HeroTextTransparent>For</HeroTextTransparent>
-          <HeroTextSolid>Engineers</HeroTextSolid>
-        </HeroText>
-
-        <WishlistBar />
+        <Grid sx={{ width: "90%" }} container>
+          <Grid item xs={12} sm={6}>
+            <HeroText>
+              <HeroTextTransparent>The</HeroTextTransparent>
+              <HeroTextTransparent>Ultimate</HeroTextTransparent>
+              <HeroTextSolid>Dev Utility</HeroTextSolid>
+            </HeroText>
+            <WishlistBar />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <ReactPlayerStyle
+              loop={true}
+              muted={true}
+              playing={true}
+              url="/demo.mp4"
+              width="100%"
+              height="100%"
+            />
+          </Grid>
+        </Grid>
       </HeroStyle>
     </HomeStyle>
   );
