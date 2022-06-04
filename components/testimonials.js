fce6450b059b6ef1ce5c6fd9162975dabcea979a
Gyana Ranjan panda <83230804+lonewolf2002@users.noreply.github.com>
2022-06-05 01:46:17 +0530

    made it responsive

--- components/testimonials.js
+++ components/testimonials.js
@@ -99,22 +99,24 @@ const Testimonial = () => {
   return (
     <Container>
       <Swiper
-        slidesPerView="3"
-        spaceBetween={30}
         slidesPerGroup={1}
         loop={true}
         loopFillGroupWithBlank={true}
         breakpoints={{
+          450: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
           640: {
             slidesPerView: 2,
             spaceBetween: 20,
           },
           768: {
-            slidesPerView: 4,
+            slidesPerView: 3,
             spaceBetween: 40,
           },
           1024: {
-            slidesPerView: 5,
+            slidesPerView: 3,
             spaceBetween: 50,
           },
         }}
