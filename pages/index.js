7a549b0364359496c294326282c28c4da487d8c0
Gyana Ranjan panda <83230804+lonewolf2002@users.noreply.github.com>
2022-06-26 16:39:16 +0530

    added favicon

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function App() {
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="language" content="English" />
 
-        <link rel="shortcut icon" href="/static/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <Home />
       <UseCasesPage />
